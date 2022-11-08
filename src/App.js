3889b88702b7e60b390b3c76d57715589be41ef9
evanlu1134 <evanlu1134@gmail.com>
2022-11-08 14:37:43 -0500

    fixed the repo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import Button from './components/button.jsx';
 import './App.css';
+import { useState } from "react"
 
 function App() {
+  let [size, setTextSize] = useState(24)
+  let [text, setTextLanguage] = useState("Good Morning!")
+  let pageSizing = 2
+  const zoomIn = () => {
+    console.log(size)
+    setTextSize(size += pageSizing)
+  }
+  const zoomOut = () => {
+    console.log(size)
+    setTextSize(size -= pageSizing)
+    
+  }
+  const getLanguage = (langaugeText) => {
+    setTextLanguage(langaugeText)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    //- and + buttons
+    <div id="body">
+      <div>
+        <Button
+          value="-"
+          event={zoomOut}
+        />
+        <Button
+          value="+"
+          event={zoomIn}
+        />
+      </div>
+      <h1 style={{ fontSize: size }}>{text}</h1>
+      {/* all langauges and button events */}
+      <div className="langauges">
+        <Button
+          value="English"
+          //execution flow in passing between getLanguage function expression
+          event={function () { getLanguage("Good Morning") }}
+        />
+        <Button
+          value="Chinese"
+          event={() => { getLanguage("早上好") }}
+        />
+        <Button
+          value="Spanish"
+          event={() => { getLanguage("Buenos días") }}
+        />
+        <Button
+          value="Haitian"
+          event={() => { getLanguage("Bonjou") }}
+        />
+        <Button
+          value="Portuguese"
+          event={() => { getLanguage("Dom Dia") }}
+        />
+      </div>
     </div>
   );
 }
--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,7 +1,7 @@
 export default function Button(props) {
     return (
-        <button onClick = {()=>(props.event())}>
-        {props.value}
+        <button onClick={props.event}>
+            {props.value}
         </button>
 
     )
